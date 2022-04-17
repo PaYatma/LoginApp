c88618f157b7b2239c235b32eb7b39f4c92728ee
Yatma <yatma27@gmail.com>
2022-04-18 01:39:51 +0200

    add sql to postgres url

--- api.py
+++ api.py
@@ -29,7 +29,7 @@ app.permanent_session_lifetime = timedelta(minutes=10)
 
 # Create engine
 engine_options = app.config['SQLALCHEMY_ENGINE_OPTIONS']
-url = os.environ.get('DATABASE_URL')
+url = os.environ.get('DATABASE_URL').replace('postgres', 'postgresql')
 engine = db.create_engine(sa_url=url, engine_opts=engine_options)
 
 # Login settings
