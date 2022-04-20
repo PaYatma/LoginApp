0ff9e4c6a850b215d64966688eae178673738ac8
Yatma <yatma27@gmail.com>
2022-04-20 15:56:55 +0200

    updates

--- scripts/create_user_tab.py
+++ scripts/create_user_tab.py
@@ -22,14 +22,9 @@ conn = psycopg2.connect(host="ec2-99-80-170-190.eu-west-1.compute.amazonaws.com"
 # Create a cursor in order to execute query into the database
 mycursor = conn.cursor()
 
-# Drop the table if it already exists
-#mycursor.execute("DROP TABLE IF EXISTS User;")
+mycursor.execute("ALTER TABLE User RENAME TO Old_User;")
 
-# Create a new table called Documents
-mycursor.execute("""CREATE TABLE "User" (ID SERIAL PRIMARY KEY, firstname VARCHAR(50),
-                    lastname VARCHAR(50), company VARCHAR(50), country VARCHAR(50), 
-                    email VARCHAR(50) unique, password VARCHAR(120), confirm_email BOOLEAN NOT NULL,
-                    created_date DATE NOT NULL)""")
+
     
 # Push (or commit) our queries into the database in order to view changes
 conn.commit()
