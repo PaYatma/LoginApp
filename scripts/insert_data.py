649f5089f233dda42e07a0cfa3f501c5d8da350d
Yatma <yatma27@gmail.com>
2022-04-21 15:12:44 +0200

    :fire: update insert.py file

--- scripts/insert_data.py
+++ scripts/insert_data.py
@@ -1,6 +1,8 @@
 # Load needed packages 
+import os
 import pandas as pd
-import mysql.connector
+'''import mysql.connector'''
+import psycopg2
 
 # Read data from "Codes/data/" folder
 mydf = pd.read_csv("data/global_tab.csv")
@@ -13,7 +15,7 @@ mydf["Note"].fillna(".", inplace=True)
 mytuples = [tuple(mydf.iloc[i]) for i in range(mydf.shape[0])]
 
 # Convert variables type in mysql
-class NumpyMySQLConverter(mysql.connector.conversion.MySQLConverter):
+'''class NumpyMySQLConverter(mysql.connector.conversion.MySQLConverter):
     """ A mysql.connector Converter that handles Numpy types """
 
     def _float32_to_mysql(self, value):
@@ -26,16 +28,19 @@ class NumpyMySQLConverter(mysql.connector.conversion.MySQLConverter):
         return int(value)
 
     def _int64_to_mysql(self, value):
-        return int(value)
+        return int(value)'''
 
 # Connect to the DATABASE
-conn = mysql.connector.connect(host="eu-cdbr-west-02.cleardb.net",
+"""conn = mysql.connector.connect(host="eu-cdbr-west-02.cleardb.net",
                         user="b22cde7263420f",
                         password="4703565c",
-                        database="heroku_963b7cc12121e0d")
+                        database="heroku_963b7cc12121e0d")"""
+                        
+DATABASE_URL = os.getenv("DATABASE_URL").replace("postgres://", "postgresql://")
+conn = psycopg2.connect(DATABASE_URL)
 
 # Add the conversion
-conn.set_converter_class(NumpyMySQLConverter)
+#conn.set_converter_class(NumpyMySQLConverter)
 
 # Create a cursor in order to execute query into the database
 mycursor = conn.cursor()
@@ -49,8 +54,8 @@ mycursor.execute("CREATE TABLE Documents (ID SERIAL PRIMARY KEY,"
         "Created Date, Submission VARCHAR(50), Documents Text, Note Text)")
 
 # Insert data into our table
-mycursor.executemany("INSERT INTO Documents (Code, Country, Study, Tag, Created, "
-                        "Submission, Documents, Note) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)", mytuples)
+mycursor.executemany("""INSERT INTO Documents (Code, Country, Study, Tag, Created,
+                        Submission, Documents, Note) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)""", mytuples)
     
 # Push (or commit) our queries into the database in order to view changes
 conn.commit()
